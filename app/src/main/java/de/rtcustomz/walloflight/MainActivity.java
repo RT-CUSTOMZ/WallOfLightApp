16c53757eabb1b52a903a3071b0d6544713e2c41
Krathegon <fabian.kalkofen@gmail.com>
2015-11-19 20:39:18 +0100

    - added imageviewer and button for choosing picture - show resulting bitmap with resolution of 88x88 pixel

--- app/src/main/java/de/rtcustomz/walloflight/MainActivity.java
+++ app/src/main/java/de/rtcustomz/walloflight/MainActivity.java
@@ -1,6 +1,14 @@
 package de.rtcustomz.walloflight;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.service.media.MediaBrowserService.Result;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,8 +16,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    static final int CHOOSE_PICTURE_REQUEST = 1;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +32,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        Button choosePictureButton = (Button) findViewById(R.id.choosePictureButton);
 
         /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -48,5 +67,34 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CHOOSE_PICTURE_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Uri imageUri = data.getData();
+                //imageView.setImageURI(imageUri);
+
+                try {
+                    Bitmap image = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+                    imageView.setImageBitmap(Bitmap.createScaledBitmap(image, 88, 88, false));
+                } catch (IOException ignore) {}  // someone was very stupid ...
+
+            }
+        }
+    }
+
+    public void selectPicture(View view) {
+        Intent imageIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        imageIntent.setType("image/*");
+
+        PackageManager packageManager = getPackageManager();
+        List activities = packageManager.queryIntentActivities(imageIntent, PackageManager.MATCH_DEFAULT_ONLY);
+        boolean isIntentSafe = activities.size() > 0;
+
+        if(isIntentSafe) {
+            startActivityForResult(Intent.createChooser(imageIntent, "Bild auswählen"), CHOOSE_PICTURE_REQUEST);
+        }
     }
 }
