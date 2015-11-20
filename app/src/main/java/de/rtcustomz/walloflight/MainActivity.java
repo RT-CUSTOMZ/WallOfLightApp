6f55607591632e476c3f2886cba4156c7007edbb
Dennis Ahrens <dennis.ahrens@gmx.net>
2015-11-20 16:02:15 +0100

    added first version of artdmx output

--- app/src/main/java/de/rtcustomz/walloflight/MainActivity.java
+++ app/src/main/java/de/rtcustomz/walloflight/MainActivity.java
@@ -1,10 +1,13 @@
 package de.rtcustomz.walloflight;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -18,6 +21,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.util.List;
@@ -25,6 +30,8 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     static final int CHOOSE_PICTURE_REQUEST = 1;
     private ImageView imageView;
+    private Bitmap scaledImage = null;
+    private Button sendbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,17 +41,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         imageView = (ImageView) findViewById(R.id.imageView);
-
-        Button choosePictureButton = (Button) findViewById(R.id.choosePictureButton);
-
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
+        sendbutton = (Button) findViewById(R.id.sendPacketsButton);
     }
 
     @Override
@@ -78,8 +75,9 @@ public class MainActivity extends AppCompatActivity {
 
                 try {
                     Bitmap image = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
-                    Bitmap scaledImage = Bitmap.createScaledBitmap(image, 88, 88, false);
+                    scaledImage = Bitmap.createScaledBitmap(image, 88, 88, false);
                     //imageView.setImageBitmap(Bitmap.createScaledBitmap(image, 88, 88, false));
+                    sendbutton.setVisibility(Button.VISIBLE);
                 } catch (IOException ignore) {}  // someone was very stupid ...
 
             }
@@ -96,6 +94,31 @@ public class MainActivity extends AppCompatActivity {
 
         if(isIntentSafe) {
             startActivityForResult(Intent.createChooser(imageIntent, "Bild auswählen"), CHOOSE_PICTURE_REQUEST);
+        }
+    }
+
+    public void sendUDPPacket(View view) {
+        //
+        boolean wifiConnected;
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeInfo = connMgr.getActiveNetworkInfo();
+        if (activeInfo != null && activeInfo.isConnected()) {
+            wifiConnected = activeInfo.getType() == ConnectivityManager.TYPE_WIFI;
+        } else {
+            wifiConnected=false;
+        }
+        //
+        if(wifiConnected) {
+            if(scaledImage!=null) {
+                Thread t = new Thread(new Client(scaledImage));
+                t.start();
+            }
+            else {
+                Toast.makeText(getApplicationContext(), "Fehler mit Bild", Toast.LENGTH_LONG).show();
+            }
+        }
+        else {
+            Toast.makeText(getApplicationContext(), "Kein WLAN", Toast.LENGTH_LONG).show();
         }
     }
 }
