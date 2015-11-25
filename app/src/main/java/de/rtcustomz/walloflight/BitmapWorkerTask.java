d8fa4eca11a0d9a0510e5282bff0df142d69686b
Krathegon <fabian.kalkofen@gmail.com>
2015-11-25 18:34:04 +0100

    - added rotation of image - load image more efficiently

--- app/src/main/java/de/rtcustomz/walloflight/BitmapWorkerTask.java
+++ app/src/main/java/de/rtcustomz/walloflight/BitmapWorkerTask.java
@@ -1,13 +1,17 @@
 package de.rtcustomz.walloflight;
 
 import android.content.ContentResolver;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.provider.MediaStore;
 import android.widget.ImageView;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.lang.ref.WeakReference;
 import java.nio.ByteBuffer;
 
@@ -15,7 +19,6 @@ public class BitmapWorkerTask extends AsyncTask<Uri, Void, Bitmap> {
     private final WeakReference<Bitmap> scaledImageReference;
     private final WeakReference<ContentResolver> contentResolverReference;
     private final WeakReference<ImageView> imageViewReference;
-    private Uri imageUri;
     private Bitmap image;
 
     public BitmapWorkerTask(Bitmap scaledImage, ImageView imageView, ContentResolver contentResolver) {
@@ -29,26 +32,21 @@ public class BitmapWorkerTask extends AsyncTask<Uri, Void, Bitmap> {
         // show image if ready to show
         final ImageView imageView = imageViewReference.get();
 
-        imageView.setImageURI(imageUri);
+        imageView.setImageBitmap(image);
     }
 
     @Override
     protected Bitmap doInBackground(Uri... params) {
-        imageUri = params[0];
-
-        // we can show image, because we have the path to it
-        publishProgress();
+        Uri imageUri = params[0];
 
         Bitmap scaledImage = null;
 
         try {
-            image = MediaStore.Images.Media.getBitmap(contentResolverReference.get(), imageUri);
+            //image = MediaStore.Images.Media.getBitmap(contentResolverReference.get(), imageUri);
+            image = decodeBitmapFromUri(imageUri, 1000, 1000);
 
-            // TODO: scale image, because we don't want to show full sized image in imageView
-            // TODO: rotate image if wrong orientation
-
-            //ExifInterface exif = new ExifInterface(imageUri.getPath());
-            //int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+            // we can show image, because we have decoded it
+            publishProgress();
 
             scaledImage = Bitmap.createScaledBitmap(image, 88, 88, false);
 
@@ -60,6 +58,7 @@ public class BitmapWorkerTask extends AsyncTask<Uri, Void, Bitmap> {
     @Override
     protected void onPostExecute(Bitmap bitmap) {
         if (bitmap != null) {
+            //TODO: that's not very efficient...
             final Bitmap scaledBitmap = scaledImageReference.get();
 
             int bytes = bitmap.getByteCount();
@@ -70,5 +69,80 @@ public class BitmapWorkerTask extends AsyncTask<Uri, Void, Bitmap> {
 
             scaledBitmap.copyPixelsFromBuffer(buffer);
         }
+    }
+
+    private int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) > reqHeight && (halfWidth / inSampleSize) > reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
+    }
+
+    private Bitmap decodeBitmapFromUri(Uri imageUri, int reqWidth, int reqHeight) throws IOException {
+        ContentResolver contentResolver = contentResolverReference.get();
+
+        if(contentResolver == null) {
+            throw new IOException("There was an error opening the image, please try again later!");
+        }
+
+        InputStream is = contentResolver.openInputStream(imageUri);
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeStream(is, null, options);
+        is.close();
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+
+        Bitmap srcBitmap;
+        is = contentResolver.openInputStream(imageUri);
+
+        srcBitmap = BitmapFactory.decodeStream(is, null, options);
+
+        is.close();
+
+        int orientation = getOrientation(contentResolver, imageUri);
+
+        if (orientation > 0) {
+            Matrix matrix = new Matrix();
+            matrix.postRotate(orientation);
+
+            srcBitmap = Bitmap.createBitmap(srcBitmap, 0, 0, srcBitmap.getWidth(), srcBitmap.getHeight(), matrix, true);
+        }
+        return srcBitmap;
+    }
+
+    private int getOrientation(ContentResolver contentResolver, Uri photoUri) {
+        Cursor cursor = contentResolver.query(photoUri,
+                new String[] { MediaStore.Images.ImageColumns.ORIENTATION }, null, null, null);
+
+        if (cursor == null || cursor.getCount() != 1) {
+            return -1;
+        }
+
+        cursor.moveToFirst();
+        int orientation = cursor.getInt(0);
+        cursor.close();
+
+        return orientation;
     }
 }
